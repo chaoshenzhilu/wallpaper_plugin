30876ab1854f43bf6797c40eca3a1526bd57ef15
超神 <790166359@qq.com>
2019-12-03 11:20:09 +0800

    截图保存

--- ios/Classes/SwiftWallpaperPlugin.swift
+++ ios/Classes/SwiftWallpaperPlugin.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 
 public class SwiftWallpaperPlugin: NSObject, FlutterPlugin {
+  var result: FlutterResult?;
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "wallpaper_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftWallpaperPlugin()
@@ -9,6 +10,27 @@ public class SwiftWallpaperPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    self.result = result
+    if call.method == "saveImageToFile" {
+      guard let imageData = (call.arguments as? FlutterStandardTypedData)?.data, let image = UIImage(data: imageData) else { return }
+      UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
+    }else {
+        result(FlutterMethodNotImplemented)
+    }
+  }
+  /// finish saving，if has error，parameters error will not nill
+  @objc func didFinishSavingImage(image: UIImage, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
+      result?(error == nil)
+  }
+  @objc func didFinishSavingVideo(videoPath: String, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
+      result?(error == nil)
+  }
+
+  func isImageFile(filename: String) -> Bool {
+      return filename.hasSuffix(".jpg")
+          || filename.hasSuffix(".png")
+          || filename.hasSuffix(".JPEG")
+          || filename.hasSuffix(".JPG")
+          || filename.hasSuffix(".PNG")
   }
 }
